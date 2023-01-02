83015b01c5aececafaafe353b7f46c12f61161bf
nelson <nelson3chege@gmail.com>
2023-01-02 16:07:37 +0300

    feat: implement stat section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { stats } from '../constants'
+import styles from '../style'
 
 export default function Stats() {
   return (
-    <div>Stats</div>
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+      {stats.map(stat =>(
+        <div key={stat.id} className='flex-1 flex justify-start items-center'>
+          <h4 className='font-poppins font-semibold xs:text-[40px] text-[30px] 
+          xs:leading-[53px] leading-[43px] text-gradient'>{stat.value}</h4>
+          <p className='font-poppins font-normal xs:text-[20px] text-[15px]
+           xs:leading-[26px] leading-[43px] text-white uppercase ml-3'>{stat.title}</p>
+        </div>
+      ))}
+    </section>
   )
 }
